41f78d52e40bd73aaa9c4a8b0840bb265553acaf
Muhammad Ashary <ariashary92@gmail.com>
2019-12-06 17:00:04 +0700

    Try update data

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,6 +43,20 @@ exports.delete = function(req, res, next) {
     });
 };
 
+exports.update = function(req, res, next) {
+    const userId = req.params.id;
+    const connection = getConnection();
+    const queryString = "SELECT * FROM users WHERE id = ?";
+    connection.query(queryString, [userId], (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to query for users: " + err);
+            res.sendStatus(500);
+            res.end();
+        }
+        res.render('users/update', { data: rows });
+    });
+};
+
 exports.detail = function(req, res, next) {
     const userId = req.params.id;
     const connection = getConnection();
@@ -57,6 +71,26 @@ exports.detail = function(req, res, next) {
     });
 };
 
+exports.user_update = function(req, res, next) {
+    let userId = req.params.id;
+    const full_name = req.body.full_name.toUpperCase();
+    const position = req.body.position;
+    const department = req.body.department;
+    const user_id = req.body.user_id;
+    const password = req.body.password;
+    const address = req.body.address;
+
+    const queryString = "INSERT INTO users (full_name, position, department, user_id, password, address) VALUES (?, ?, ?, ?, ?, ?) WHERE id = ?";
+        getConnection().query(queryString, [full_name, position, department, user_id, password, address], [userId], (err, result, fields) => {
+            if (err) {
+                console.log("Failed to insert new users: " + err);
+                res.sendStatus(500);
+                return
+            }
+    });
+    // res.redirect('/users?message=Successfully updated user!');
+};
+
 exports.user_create = function(req, res, next) {
     const full_name = req.body.full_name.toUpperCase();
     const position = req.body.position;
@@ -67,7 +101,7 @@ exports.user_create = function(req, res, next) {
     const address = req.body.address;
 
     if (password === confirm_password) {
-        const queryString = "INSERT INTO users (full_name, position, department, user_id, password, address) VALUES (?, ?, ?, ?, ?, ?)";
+        const queryString = "UPDATE users SET (full_name, position, department, user_id, password, address) VALUES (?, ?, ?, ?, ?, ?)";
         getConnection().query(queryString, [full_name, position, department, user_id, password, address], (err, result, fields) => {
             if (err) {
                 console.log("Failed to insert new users: " + err);
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,8 @@ var user_controller = require('../controllers/userController.js');
 router.get('/', user_controller.index);
 router.get('/:id', user_controller.detail);
 router.get('/delete/:id', user_controller.delete);
+router.get('/update/:id', user_controller.update);
+router.post('/user-update', user_controller.user_update);
 router.post('/user-create', user_controller.user_create);
 
 module.exports = router;
